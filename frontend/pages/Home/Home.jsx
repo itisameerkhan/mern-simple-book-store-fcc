5a63d24a237b01307207f974b8929440a37ce492
itisameerkhan <itisameerkhan@gmail.com>
2024-02-10 11:05:59 +0530

    update components

--- frontend/pages/Home/Home.jsx
+++ frontend/pages/Home/Home.jsx
@@ -1,61 +1,64 @@
-import { useEffect, useState } from 'react';
-import './Home.scss';
-import { Link } from 'react-router-dom';
-import Loader from '../../components/Loader/Loader';
+import { useEffect, useState } from "react";
+import "./Home.scss";
+import { Link } from "react-router-dom";
+import Loader from "../../components/Loader/Loader";
 
 const Home = () => {
-  
   const [books, setBooks] = useState(null);
 
   useEffect(() => {
-    const getData = async() => {
-      const data = await fetch('http://localhost:5555/books');
+    const getData = async () => {
+      const data = await fetch("http://localhost:5555/books");
       const json = await data.json();
       setBooks(json);
-    }
+    };
     getData();
-  },[]);
+  }, []);
 
-  if(!books) return <Loader />
+  if (!books) return <Loader />;
 
   return (
     <div className="home">
       <div className="home-t">
-        <h1 className='title'>Books</h1>
-        <Link to={'/books/create'}>
-        <span className="material-symbols-outlined">add_box</span>
+        <h1 className="title">Books</h1>
+        <Link to={"/books/create"}>
+          <span className="material-symbols-outlined">add_box</span>
         </Link>
       </div>
-      <table className='books-table'>
-        <tr>
-          <th>No</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Published Year</th>
-          <th>Operations</th>
-        </tr>
-        {books?.data.map((val, index) => (
-          <tr key={index}>
-            <td>{index+1}</td>
-            <td>{val?.title}</td>
-            <td>{val?.author}</td>
-            <td>{val?.publishYear}</td>
-            <td className='operations'>
-            <Link to={`/books/details/${val._id}`}>
-              <span className="material-symbols-outlined">info</span>
-            </Link>
-            <Link to={`/books/edit/${val._id}`}>
-              <span className="material-symbols-outlined">edit</span>
-            </Link>
-            <Link to={`/books/delete/${val._id}`}>
-              <span className="material-symbols-outlined">delete</span>
-            </Link>
-            </td>
+      <table className="books-table">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Published Year</th>
+            <th>Operations</th>
           </tr>
+        </thead>
+        {books?.data.map((val, index) => (
+          <tbody key={index}>
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{val?.title}</td>
+              <td>{val?.author}</td>
+              <td>{val?.publishYear}</td>
+              <td className="operations">
+                <Link to={`/books/details/${val._id}`}>
+                  <span className="material-symbols-outlined">info</span>
+                </Link>
+                <Link to={`/books/edit/${val._id}`}>
+                  <span className="material-symbols-outlined">edit</span>
+                </Link>
+                <Link to={`/books/delete/${val._id}`}>
+                  <span className="material-symbols-outlined">delete</span>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
         ))}
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
